01b4c1922b9a1c1fd8ce39e6fc9720bdae1177f1
Renan Damascena Pereira <renandamascena@gmail.com>
2023-05-19 20:06:50 -0300

    fix: checkBookingRouter

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { bookingRoom, changeBooking, listBooking } from '@/controllers';
+import { bookingcheck, bookingRoom, changeBooking, listBooking } from '@/controllers';
 
 const bookingRouter = Router();
 
-bookingRouter.all('/*', authenticateToken).get('', listBooking).post('', bookingRoom).put('/:bookingId', changeBooking);
+bookingRouter
+  .all('/*', authenticateToken)
+  .get('', listBooking)
+  .get('/check', bookingcheck)
+  .post('', bookingRoom)
+  .put('/:bookingId', changeBooking);
 
 export { bookingRouter };
