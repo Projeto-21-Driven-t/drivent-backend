dc23e5c066b204c59d6029135b4ab80928cae374
Renan Damascena Pereira <renandamascena@gmail.com>
2023-05-25 17:09:28 -0300

    commit inutil

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,9 @@
-import { createClient } from "redis";
+import { createClient } from 'redis';
 
 const redis = createClient();
-redis.on('error', err => console.log('Redis Client Error', err))
+redis.on('error', (err) => console.log('Redis Client Error', err));
 
 const connectRedis = async () => await redis.connect();
 connectRedis();
 
-export { redis }
+export { redis };
